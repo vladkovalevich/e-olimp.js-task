26a14fed8f7317e60f3d92a21474f9805207df43
vladkovalevich <64867756+vladkovalevich@users.noreply.github.com>
2020-07-30 09:19:19 +0300

    .........

--- hm-6/t3.js
+++
@@ -1,6 +0,0 @@
-import users from "./users.js";
-
-const getUserNames = users => {
-    return users.map(user => user.name);
-};
-console.log(getUserNames(users));
--- hm-6/task-2.js
+++
@@ -1,5 +0,0 @@
-const getUsersWithEyeColor = (users, color) => {
-    return users.filter(user => user.eyeColor === color);
-};
-
-console.log(getUsersWithEyeColor(users, "blue")); 
